ad0c3c286661e5bbdd7b39012dc2cf519f9657a6
davidchanho <davidho.crd@gmail.com>
2020-12-18 04:04:25 -0500

    add collapse to navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,14 +4,15 @@ import { Nav, Navbar as BsNavbar } from "react-bootstrap";
 function Navbar() {
   return (
     <BsNavbar expand="lg" bg="light" className="fixed-top">
-      <Nav className="mx-auto">
+      <BsNavbar.Toggle className="ml-auto" />
+      <BsNavbar.Collapse className="justify-content-end">
         <Nav.Link href="/#about">About</Nav.Link>
         <Nav.Link href="/#education">Education</Nav.Link>
         <Nav.Link href="/#work">Work</Nav.Link>
         <Nav.Link href="/#projects">Projects</Nav.Link>
         <Nav.Link href="/#tech">Favorite Tech</Nav.Link>
         <Nav.Link href="/#contacts">Contact Me</Nav.Link>
-      </Nav>
+      </BsNavbar.Collapse>
     </BsNavbar>
   );
 }
