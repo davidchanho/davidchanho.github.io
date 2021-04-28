615fa440764474f656e7e93a0b9686973bfc6ef9
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-04-28 18:33:53 -0400

    add meta data

---
+++ src/components/technologies/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Technologies() {
+    return (
+        <div className='d-flex align-items-center justify-content-between'>
+            
+        </div>
+    )
+}
+
+export default Technologies
